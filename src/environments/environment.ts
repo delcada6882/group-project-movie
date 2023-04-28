f70e524fe106093f79972bfa92b1e581faf6aac3
cole nelson <colionelson@gmail.com>
2023-04-28 09:31:39 -0600

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD28ejFyr1UEj-vyRhON3xcKAkciBF4rfs',
+    authDomain: 'mtec-movie-project.firebaseapp.com',
+    projectId: 'mtec-movie-project',
+    storageBucket: 'mtec-movie-project.appspot.com',
+    messagingSenderId: '135893341618',
+    appId: '1:135893341618:web:bc4bd5ef1cc49c9c1e00ab',
+  },
 };
-
-import { initializeApp } from 'firebase/app';
-const firebaseConfig = {
-  apiKey: 'AIzaSyD28ejFyr1UEj-vyRhON3xcKAkciBF4rfs',
-  authDomain: 'mtec-movie-project.firebaseapp.com',
-  projectId: 'mtec-movie-project',
-  storageBucket: 'mtec-movie-project.appspot.com',
-  messagingSenderId: '135893341618',
-  appId: '1:135893341618:web:bc4bd5ef1cc49c9c1e00ab',
-};
-
-const app = initializeApp(firebaseConfig);
