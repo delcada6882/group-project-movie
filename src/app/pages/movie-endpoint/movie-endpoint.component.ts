784f631fdc8e0e48afba9658d671d732c021fbeb
cole nelson <colionelson@gmail.com>
2023-05-06 16:20:59 -0600

    question mark on dot name

--- src/app/pages/movie-endpoint/movie-endpoint.component.ts
+++ src/app/pages/movie-endpoint/movie-endpoint.component.ts
@@ -7,168 +7,175 @@ import { ApiCallService } from 'src/app/services/api-call.service';
 import months from 'src/utility/constants/month';
 
 @Component({
-  selector: 'app-movie-endpoint',
-  templateUrl: './movie-endpoint.component.html',
-  styleUrls: ['./movie-endpoint.component.scss'],
+	selector: 'app-movie-endpoint',
+	templateUrl: './movie-endpoint.component.html',
+	styleUrls: ['./movie-endpoint.component.scss'],
 })
 export class MovieEndpointComponent implements OnInit {
-  constructor(
-    private ApiCallService: ApiCallService,
-    private Host: ElementRef,
-    public MenuCtrl: MenuController
-  ) { }
+	constructor(
+		private ApiCallService: ApiCallService,
+		private Host: ElementRef,
+		public MenuCtrl: MenuController
+	) {}
 
+	public urlStart = 'https://image.tmdb.org/t/p/w500';
 
+	public timeoutId: NodeJS.Timeout | undefined;
 
-  public urlStart = 'https://image.tmdb.org/t/p/w500';
+	movieData?: Movie;
 
-  public timeoutId: NodeJS.Timeout | undefined;
+	showLoading: boolean = true;
 
-  movieData?: Movie;
+	movieId?: number;
 
-  showLoading: boolean = true;
+	private activatedRoute = inject(ActivatedRoute);
 
-  movieId?: number;
+	similarMovies?: any[];
 
-  private activatedRoute = inject(ActivatedRoute);
+	revenueText?: string;
 
-  similarMovies?: any[];
+	releaseDateText?: string;
 
-  revenueText?: string;
+	backDropIcons?: Array<any> = [];
 
-  releaseDateText?: string;
+	smallerTest?: string;
 
-  backDropIcons?: Array<any> = [];
+	movieGenres?: Array<string> = [];
 
-  smallerTest?: string;
+	moneyHeader?: string = 'Budget: ';
+	moneyAmount?: string;
 
-  movieGenres?: Array<string> = [];
+	runtimeHeader?: string = 'Runtime: ';
+	runtimeData?: string;
 
-  moneyHeader?: string = 'Budget: ';
-  moneyAmount?: string;
+	ngOnInit() {
+		this.showLoading = true;
+		this.movieId = Number(
+			this.activatedRoute.snapshot.paramMap.get('movieId') || '0'
+		);
+		this.showMovie();
+		this.getSimilarMovies();
+		this.initTimeout();
+	}
 
-  runtimeHeader?: string = 'Runtime: ';
-  runtimeData?: string;
+	initTimeout() {
+		this.timeoutId = setTimeout(() => {
+			console.log('done');
+			this.MenuCtrl.swipeGesture(true);
+		}, 1000);
+	}
 
+	moneyChange() {
+		if (this.moneyHeader === 'Budget: ') {
+			this.moneyHeader = 'Revenue: ';
+			if (this.movieData?.revenue === 0) {
+				this.moneyAmount = 'N/A';
+			}
+			this.moneyAmount = this.movieData?.revenue?.toLocaleString(
+				'en-US',
+				{
+					style: 'currency',
+					currency: 'USD',
+				}
+			);
+		} else {
+			this.moneyHeader = 'Budget: ';
+			if (this.movieData?.budget === 0) {
+				this.moneyAmount = 'N/A';
+			}
+			this.moneyAmount = this.movieData?.budget?.toLocaleString('en-US', {
+				style: 'currency',
+				currency: 'USD',
+			});
+		}
+	}
+	runtimeChange() {
+		if (this.runtimeHeader === 'Runtime: ') {
+			this.runtimeHeader = 'Status: ';
+			this.runtimeData = this.movieData?.status;
+		} else {
+			this.runtimeHeader = 'Runtime: ';
+			this.runtimeData = this.movieData?.runtime?.toString() + ' minutes';
+		}
+	}
 
-  ngOnInit() {
-    this.showLoading = true;
-    this.movieId = Number(
-      this.activatedRoute.snapshot.paramMap.get('movieId') || '0'
-    );
-    this.showMovie();
-    this.getSimilarMovies();
-    this.initTimeout();
-  }
+	showMovie() {
+		this.ApiCallService.getMovieById(Number(this.movieId)).subscribe(
+			(data: Movie) => {
+				console.log(data);
+				this.movieData = data;
+				this.Host.nativeElement.style.setProperty(
+					`--backdrop-image`,
+					'url(' + this.urlStart + this.movieData.backdrop_path + ')'
+				);
+				this.Host.nativeElement.style.setProperty(
+					`--company-image-amount`,
+					this.movieData.production_companies.length.toString()
+				);
 
-  initTimeout() {
-    this.timeoutId = setTimeout(() => {
-      console.log('done');
-      this.MenuCtrl.swipeGesture(true);
-    }, 1000);
-  }
+				this.moneyAmount = this.movieData?.budget?.toLocaleString(
+					'en-US',
+					{
+						style: 'currency',
+						currency: 'USD',
+					}
+				);
+				this.runtimeData =
+					this.movieData?.runtime?.toString() + ' minutes';
+				this.releaseDateText =
+					new Date(this.movieData?.release_date)
+						.getFullYear()
+						.toString() +
+					', ' +
+					months[
+						new Date(this.movieData?.release_date).getUTCMonth()
+					] +
+					' ' +
+					new Date(this.movieData?.release_date)
+						.getUTCDate()
+						.toString();
+				if (this.movieData?.revenue === 0) {
+					this.revenueText = 'Unknown';
+				} else {
+					this.revenueText = this.movieData?.revenue?.toLocaleString(
+						'en-US',
+						{ style: 'currency', currency: 'USD' }
+					);
+				}
 
-  moneyChange() {
-    if (this.moneyHeader === 'Budget: ') {
-      this.moneyHeader = 'Revenue: ';
-      if (this.movieData?.revenue === 0) {
-        this.moneyAmount = 'N/A';
-      }
-      this.moneyAmount = (this.movieData?.revenue?.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      }))
-    } else {
-      this.moneyHeader = 'Budget: ';
-      if (this.movieData?.budget === 0) {
-        this.moneyAmount = 'N/A';
-      }
-      this.moneyAmount = (this.movieData?.budget?.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      }))
-    }
-  }
-  runtimeChange() {
-    if (this.runtimeHeader === 'Runtime: ') {
-      this.runtimeHeader = 'Status: ';
-      this.runtimeData = this.movieData?.status;
-    } else {
-      this.runtimeHeader = 'Runtime: ';
-      this.runtimeData = this.movieData?.runtime?.toString() + ' minutes';
-    }
-  }
+				for (let x = 0; x < 2; x++) {
+					this.smallerTest = this.movieData?.genres[x]?.name;
 
-  showMovie() {
-    this.ApiCallService.getMovieById(Number(this.movieId)).subscribe(
-      (data: Movie) => {
+					console.log(
+						IconPaths[this.smallerTest as keyof typeof IconPaths]
+					);
+					console.log(this.movieData.genres[x]?.name);
+					this.backDropIcons?.push(
+						IconPaths[this.smallerTest as keyof typeof IconPaths]
+					);
+					this.movieGenres?.push(this.movieData.genres[x]?.name);
+				}
 
-        console.log(data);
-        this.movieData = data;
-        this.Host.nativeElement.style.setProperty(
-          `--backdrop-image`,
-          'url(' + this.urlStart + this.movieData.backdrop_path + ')'
-        );
-        this.Host.nativeElement.style.setProperty(
-          `--company-image-amount`,
-          this.movieData.production_companies.length.toString()
-        );
+				console.log(this.backDropIcons);
 
-        this.moneyAmount = (this.movieData?.budget?.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD'
-        }))
-        this.runtimeData = this.movieData?.runtime?.toString() + ' minutes';
-        this.releaseDateText =
-          new Date(this.movieData?.release_date)
-            .getFullYear()
-            .toString() +
-          ', ' +
-          months[
-          new Date(this.movieData?.release_date).getUTCMonth()
-          ] +
-          ' ' +
-          new Date(this.movieData?.release_date)
-            .getUTCDate()
-            .toString();
-        if (this.movieData?.revenue === 0) {
-          this.revenueText = 'Unknown';
-        } else {
-          this.revenueText = this.movieData?.revenue?.toLocaleString(
-            'en-US',
-            { style: 'currency', currency: 'USD' }
-          );
-        }
+				this.showLoading = false;
+			}
+		);
+	}
 
-        for (let x = 0; x < 2; x++) {
-          this.smallerTest = this.movieData?.genres[x].name;
+	testFunc() {
+		console.log('test');
+		this.MenuCtrl.swipeGesture(false);
+		clearTimeout(this.timeoutId);
+		this.initTimeout();
+	}
 
-          console.log(IconPaths[this.smallerTest as keyof typeof IconPaths])
-          console.log(this.movieData.genres[x].name)
-          this.backDropIcons?.push(IconPaths[this.smallerTest as keyof typeof IconPaths]);
-          this.movieGenres?.push(this.movieData.genres[x].name)
-        }
-
-        console.log(this.backDropIcons);
-
-        this.showLoading = false;
-      }
-    );
-  }
-
-  testFunc() {
-    console.log('test');
-    this.MenuCtrl.swipeGesture(false);
-    clearTimeout(this.timeoutId);
-    this.initTimeout();
-  }
-
-  getSimilarMovies() {
-    this.ApiCallService.getSimilarMovies(Number(this.movieId)).subscribe(
-      (data: any) => {
-        console.log(data);
-        this.similarMovies = data.results;
-      }
-    );
-  }
+	getSimilarMovies() {
+		this.ApiCallService.getSimilarMovies(Number(this.movieId)).subscribe(
+			(data: any) => {
+				console.log(data);
+				this.similarMovies = data.results;
+			}
+		);
+	}
 }
